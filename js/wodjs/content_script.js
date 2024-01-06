2de6acde83dad7493068c41d67867e38da087b1e
Claud Xiao <claud.xiao@outlook.com>
2024-01-07 00:24:24 +0800

    Format almost done.

--- js/wodjs/content_script.js
+++ js/wodjs/content_script.js
@@ -21,18 +21,63 @@ function replaceImageUrls() {
   });
 }
 
-function otherRefinement(){
+function otherRefinement() {
   //Remove the action of top tools
-  $('#gadgettable-top-gadgets .gadget_icon img').removeAttr('onmouseout').removeAttr("onmouseover");
-  $('#gadgettable-top-gadgets .gadget_icon img').off('mouseout').off("mouseover");
-  $('img[src*="images/icons/inf.gif"]').css('border-radius', '8px').css('margin-left','5px');
+  //$('#gadgettable-top-gadgets .gadget_icon img').removeAttr('onmouseout').removeAttr("onmouseover");
+  //$('#gadgettable-top-gadgets .gadget_icon img').off('mouseout').off("mouseover");
+  $('img[src*="images/icons/inf.gif"]').css('border-radius', '8px').css('margin-left', '5px');
+
+  //Gold
+  $('.gold').css('margin-top','10px').css('padding-top','10px').css('border-top','1px solid #aaa');
+  $('.gold .image').hide();
+  // fetch ep/fame
+  var epinfo = $('.ep .bar').attr('onmouseover');
+  var fameinfo = $('.fame .bar').attr('onmouseover');
+
+  // Extract the values using a regular expression
+  var totalExp = extractValue(epinfo, '总共:');
+  var usableExp = extractValue(epinfo, '可使用:');
+  var nextLevelExp = extractValue(epinfo, '到下一级别需要<b使用</b>:');
+  $('div.ep .image').hide();
+  $('div.ep .bar').hide();
+
+  $('div.ep').append('<div style="display:flex;align-items: center;margin-top:5px;"><div style="margin-right:8px;">Exp: </div><div class="experience-bar" hin><div class="filled-bar"></div></div></div>')
+  updateExperienceBar(usableExp, nextLevelExp, '.experience-bar');
+  // Extract the values using regular expressions
+  var totalFame = extractValue(fameinfo, '荣誉总数（含奖励值）:');
+  var originalFame = extractValue(fameinfo, '荣誉（原始值）:');
+  var nextTitleFame = extractValue(fameinfo, '下一头衔:');
+  var missingFame = extractValue(fameinfo, '缺少:');
+  $('div.fame .image').hide();
+  $('div.fame .bar').hide();
+  $('div.fame').append('<div style="display:flex;align-items: center;margin-top:5px;margin-bottom:20px;"><div style="margin-right:8px;">Fame: </div><div style="margin-left:20px;"><span style="background:yellow;">'+missingFame+'</span> to next level.</div>')
+}
+
+// Function to extract value based on the label
+function extractValue(htmlString, label) {
+  var regex = new RegExp(label + '</td><td>([\\d,]+)');
+  var match = htmlString.match(regex);
+  if (match && match.length === 2) {
+    return match[1].replace(',', ''); // Remove commas from the number
+  }
+
+  return null;
+}
+
+// Function to update the experience bar
+function updateExperienceBar(currentExp, nextLevelExp, hdlstr) {
+  var percentage = (currentExp / nextLevelExp) * 100;
+  $(hdlstr  +' .filled-bar').css('width', percentage + '%');
+  // Update tooltip text
+  var tooltipText = `Current: ${currentExp}\nNext Level: ${nextLevelExp}`;
+  $(hdlstr).attr('title', tooltipText);
 }
 
 replaceCssFile();
-replaceImageUrls();
+//replaceImageUrls();
 otherRefinement();
 
 
 // To replace the logo with Avator
-$('.gadget.logo .gadget_body').append("<img style='width:120px;height:120px;' src='"+$('.hero_avatar.image img').attr('src')+"'>");
+$('.gadget.logo .gadget_body').append("<img style='width:120px;height:120px;' src='" + $('.hero_avatar.image img').attr('src') + "'>");
 $('.hero_avatar.image img').hide();
