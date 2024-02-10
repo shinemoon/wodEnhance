f523b06b22d343e051d186830125ab8167e55f2f
Claud Xiao <claud.xiao@outlook.com>
2024-02-10 22:04:09 +0800

    Almost there

--- js/reportGen.js
+++ js/reportGen.js
@@ -1,7 +1,15 @@
 //document.addEventListener('DOMContentLoaded', function () {
+var debug = true;
 $(document).ready(function () {
     console.log('Load ReportGen');
-    if (typeof port === 'undefined') {
+    if (debug) {
+        loadLocalJSON(data => {
+            genPage({
+                action: 'equipSimulatorPage',
+                data: data
+            });
+        })
+    } else if (typeof port === 'undefined') {
         const port = chrome.runtime.connect({ name: "reportGen" });
         // Listen for responses from the service worker
         port.onMessage.addListener((message) => {
@@ -22,6 +30,7 @@ $(document).ready(function () {
 });
 
 function genPage(dat) {
+
     if (dat.action == 'skillConfigPage') {
         genSkillSetPage(dat.data);
     }
@@ -37,7 +46,19 @@ function genPage(dat) {
     if (dat.action == 'equipSimulatorPage') {
         genEquipSimulator(dat.data);
     }
-
-
 }
 
+// Function to load a local JSON file into an object
+function loadLocalJSON(callback) {
+    const jsonFile = '/debug.json'; // Replace with the actual name of your JSON file
+    const jsonUrl = chrome.runtime.getURL(jsonFile);
+
+    fetch(jsonUrl)
+        .then(response => response.json())
+        .then(data => {
+            callback(data);
+        })
+        .catch(error => {
+            console.error('Error loading JSON file:', error);
+        });
+}
--- js/reportGen_equipSimulator.js
+++ js/reportGen_equipSimulator.js
@@ -1,6 +1,94 @@
+var eList = null;
 function genEquipSimulator(dat) {
     console.log('//To Start create the fancy table!!');
     var datInfo = dat;
     console.log(datInfo);
-    //To Start create the fancy table!!
+
+    var matrixHtml = '<div id="tableMatrix">';
+    $('body').append(matrixHtml);
+
+
+    eList = dat[2];
+
+    function getFirstKeyEndingWithName(obj) {
+        for (const key in obj) {
+            if (obj.hasOwnProperty(key) && key.endsWith('Name')) {
+                return key;
+            }
+        }
+        return undefined; // Return undefined if no matching key is found
+    }
+
+    function refineReqToString(objArray) {
+        if (objArray === undefined) return "";
+        return objArray.map(item => {
+            if (item == null) return "";
+            return "<div>" + item.name + item.rule + item.threshold + "</div>";
+        }).join("");
+    }
+
+    function refineAddToString(objArray) {
+        if (objArray === undefined) return "";
+        return objArray.map(item => {
+            if (item == null) return "";
+            return "<div>" + item.name + ":" + item.value + "</div>";
+        }).join("");
+
+    }
+
+
+    var i = 0;
+    eList.forEach(element => {
+        i = i + 1;
+        //To Start create the fancy table!!
+        var tableHtml = '<table id="equipEmulator-' + i + '" class="equipEmulator">';
+        $('#tableMatrix').append(tableHtml);
+
+        var tableItem = $('#equipEmulator-' + i);
+        tableItem.append("<tr class='tbhead'>");
+        $('.tbhead').last().append("<th>装备</th>");
+        $('.tbhead').last().append("<th>要求</th>");
+        $('.tbhead').last().append("<th>效果</th>");
+
+        var j = 0;
+        element.forEach(curItem => {
+            j = j + 1;
+            if (refineReqToString(curItem.req) != "" || refineAddToString(curItem.attrAdd) != "") {
+                $('#equipEmulator-' + i).append("<tr class='equiprow'>")
+                $('.equiprow').last().append("<td class='name' tabind=" + (i - 1) + " itemind = " + (j - 1) + ">" + curItem[getFirstKeyEndingWithName(curItem)] + "</td>");
+                $('.equiprow').last().append("<td>" + refineReqToString(curItem.req) + "</td>");
+                $('.equiprow').last().append("<td>" + refineAddToString(curItem.attrAdd) + "</td>");
+            }
+        })
+    });
+
+    $('td.name').hover(function () {
+        if ($(this).hasClass('selected')) return;
+        //mouseover
+        $(this).css('background-color', '#e0e0e0');
+    }, function () {
+        if ($(this).hasClass('selected')) return;
+        //mouseout
+        $(this).css('background-color', '#f0f0f0');
+    })
+
+
+    $('td.name').click(function () {
+        if ($(this).hasClass('selected')) {
+            $(this).removeClass('selected');
+            $('tr').removeClass('fade');
+        } else {
+            $('td.name.selected').removeClass('selected');
+            calculateRelations($(this).attr('tabind'), $(this).attr('itemind'));
+            $(this).addClass('selected');
+            $('tr').addClass('fade');
+            $(this).parent().removeClass('fade');
+        }
+    });
+
+    function calculateRelations(ti, ii) {
+        console.log(ti + ":" + ii);
+        console.log(eList[ti][ii]);
+    }
+
 };
